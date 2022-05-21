291c2e5209cfc31c376899f619f5079d061973fa
thunderkyg <thunderkyg@gmail.com>
2022-05-21 23:02:14 +0900

    백준단계별로

---
+++ src/baekjoon/input/arithmetic/Question1.java
@@ -0,0 +1,11 @@
+package baekjoon.input.arithmetic;
+//채점할 때 사용하는 파일명은 Main.java입니다. 따라서, main 메소드는 Main 클래스에 있어야 합니다.
+class Question1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		
+	}
+
+}
